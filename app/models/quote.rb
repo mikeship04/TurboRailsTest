b4fe6bcd8ccda4978291d757b552c593820630a1
Mike Shippy <mikeship04@gmail.com>
2022-11-08 15:39:18 -0700

    final

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,7 @@ class Quote < ApplicationRecord
     validates :name, presence: true
     belongs_to :company
     has_many :line_item_dates, dependent: :destroy
+    has_many :line_items, through: :line_item_dates
 
     scope :ordered, -> { order(id: :desc)}
 
@@ -9,4 +10,8 @@ class Quote < ApplicationRecord
     # after_update_commit -> { broadcast_replace_later_to "quotes" }
     # after_destroy_commit -> { broadcast_remove_to "quotes" }
     broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+    def total_price
+        line_items.sum(&:total_price)
+    end
 end
